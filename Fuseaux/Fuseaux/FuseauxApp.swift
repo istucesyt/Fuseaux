8dac515677649acf4672bcbd69c00067c8341d3b
iStuces <108399865+istucesyt@users.noreply.github.com>
2023-06-25 16:48:34 +0200

    Code source de Fuseaux

---
+++ Fuseaux/Fuseaux/FuseauxApp.swift
@@ -0,0 +1,23 @@
+//
+//  FuseauxApp.swift
+//  Fuseaux
+//
+//  Created by Lucas Drouot on 09/06/2023.
+//
+
+import SwiftUI
+import AppKit
+import Foundation
+import WebKit
+import ServiceManagement
+import Cocoa
+
+@main
+struct FuseauxApp: App {
+
+    var body: some Scene {
+        WindowGroup {
+            ContentView()
+        }
+    }
+}
