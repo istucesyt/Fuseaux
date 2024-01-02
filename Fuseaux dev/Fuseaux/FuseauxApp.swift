ec378ce8bde64dcb22b00dc8cdf454745d16a939
iStuces <108399865+istucesyt@users.noreply.github.com>
2024-01-02 12:01:30 +0100

    Add files via upload

---
+++ Fuseaux dev/Fuseaux/FuseauxApp.swift
@@ -0,0 +1,43 @@
+//
+//  FuseauxApp.swift
+//  Fuseaux
+//
+//  Created by iStuces on 09/06/2023.
+//
+
+import SwiftUI
+import AppKit
+import Foundation
+import WebKit
+import ServiceManagement
+import Cocoa
+
+@main
+struct FuseauxApp: App {
+    var body: some Scene {
+        WindowGroup {
+            ContentView()
+                .background(TitleBarWindowStyle())
+        }
+        .windowStyle(HiddenTitleBarWindowStyle())
+    }
+}
+
+struct TitleBarWindowStyle: NSViewRepresentable {
+    func makeNSView(context: Context) -> NSView {
+        let view = NSView()
+        DispatchQueue.main.async {
+            if let window = view.window {
+                window.titleVisibility = .visible
+                window.titlebarAppearsTransparent = true
+                window.standardWindowButton(.closeButton)?.isHidden = false
+                window.standardWindowButton(.zoomButton)?.isHidden = false
+                window.standardWindowButton(.miniaturizeButton)?.isHidden = true
+                
+            }
+        }
+        return view
+    }
+    
+    func updateNSView(_ nsView: NSView, context: Context) {}
+}
